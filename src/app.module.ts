f3fd76f8bf611c2faba21a4ea1150bb05a83a414
Eduardo Ferraz <edupferraz@gmail.com>
2025-09-09 06:20:49 -0300

    feat(prisma): add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller.ts";
 import { AppService } from "./app.service.ts";
+import { PrismaService } from "./prisma/prisma.service.ts";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "generated/prisma/index.js";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: ["warn", "error"],
+    });
+  }
+
+  onModuleInit() {
+    return this.$connect;
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect;
+  }
+}
